3401a59de9a8689a34f831ff9ddd68a2522304d3
J Leadbetter <codemonkey@jleadbetter.com>
2014-12-20 11:57:00 -0500

    [REDESIGN] CSS fixes for main site

--- script/page/index.js
+++ script/page/index.js
@@ -3,17 +3,17 @@ gdiPage = {
   content: "pages/index.ect",
   chapterUrl: "http://www.girldevelopit.com/chapters/boulder",
   modules: [
-    { title: "Introduction Git & GitHub", url: "https://github.com/etmoore/gdi-git-workshop" },
-    { title: "Introduction to HTML & CSS", url: "http://gdiboulder.github.io/gdi-boulder-intro-html-css" },
-    { title: "Intermediate HTML & CSS", url: "http://gdiboulder.github.io/gdi-boulder-intermediate-htmlcss" },
-    { title: "Introduction to Javascript", url: "http://gdiboulder.github.io/gdi-intro-javascript" },
-    { title: "Introduction to Programming Concepts", url: "http://gdiboulder.github.io/gdi-boulder-intro-programming-concepts" },
-    { title: "Introduction to Bootstrap 3", url: "http://gdiboulder.github.io/gdi-boulder-intro-bootstrap" },
-    { title: "Advanced CSS Tips & Techniques", url: "http://gdiboulder.github.io/gdi-boulder-advanced-css-techniques" },
-    { title: "CSS3 Properties & Selectors", url: "http://gdiboulder.github.io/gdi-boulder-css3-properties"  },
-    { title: "Introduction to Servers", url: "http://gdiboulder.github.io/gdi-boulder-servers-intro" },
-    { title: "Introduction to Python", url: "http://gdiboulder.github.io/gdi-intro-python" },
-    { title: "Introduction to Command Line", url: "http://bit.ly/clgdi" }
+    { title: "Introduction to HTML & CSS", url: "http://gdiboulder.github.io/gdi-boulder-intro-html-css", class: "module-css" },
+    { title: "Intermediate HTML & CSS", url: "http://gdiboulder.github.io/gdi-boulder-intermediate-htmlcss", class: "module-css" },
+    { title: "Advanced CSS Tips & Techniques", url: "http://gdiboulder.github.io/gdi-boulder-advanced-css-techniques", class: "module-css" },
+    { title: "CSS3 Properties & Selectors", url: "http://gdiboulder.github.io/gdi-boulder-css3-properties", class: "module-css" },
+    { title: "Introduction to Bootstrap 3", url: "http://gdiboulder.github.io/gdi-boulder-intro-bootstrap", class: "module-css" },
+    { title: "Introduction to Programming Concepts", url: "http://gdiboulder.github.io/gdi-boulder-intro-programming-concepts", class: "module-programming" },
+    { title: "Introduction to Python", url: "http://gdiboulder.github.io/gdi-intro-python", class: "module-programming" },
+    { title: "Introduction to Javascript", url: "http://gdiboulder.github.io/gdi-intro-javascript", class: "module-programming" },
+    { title: "Introduction Git & GitHub", url: "https://github.com/etmoore/gdi-git-workshop", class: "module-tools" },
+    { title: "Introduction to Command Line", url: "http://bit.ly/clgdi", class: "module-tools" },
+    { title: "Introduction to Servers", url: "http://gdiboulder.github.io/gdi-boulder-servers-intro", class: "module-tools" }
   ]
 };
 // bring in base variables, but don't override ones defined here
